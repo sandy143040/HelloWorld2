a8d151ca677b6dda76516085b52e1e43cdd2b10b
Sandeep kumar singh <109274261+sandy143040@users.noreply.github.com>
2023-04-05 23:09:48 +0530

    Update Program.cs

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world2");
+            Console.WriteLine("code pushing from 2nd developer");
             Console.ReadLine();
         }
     }
